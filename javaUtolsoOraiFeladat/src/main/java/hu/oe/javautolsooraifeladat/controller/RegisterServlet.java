04fc152d2b8c5031e880626388c1911ebff844e9
mpeterr <m.peter.r@stud.uni-obuda.hu>
2023-05-24 18:05:15 +0200

    Set UTF-8 char encoding

--- javaUtolsoOraiFeladat/src/main/java/hu/oe/javautolsooraifeladat/controller/RegisterServlet.java
+++ javaUtolsoOraiFeladat/src/main/java/hu/oe/javautolsooraifeladat/controller/RegisterServlet.java
@@ -17,6 +17,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String fullname = request.getParameter("fullname");
