b27c8f896412f6389edc4d507708e3971ed5749c
mpeterr <m.peter.r@stud.uni-obuda.hu>
2023-05-24 17:51:03 +0200

    RegisterServlet elkészítése

--- javaUtolsoOraiFeladat/src/main/java/hu/oe/javautolsooraifeladat/controller/RegisterServlet.java
+++ javaUtolsoOraiFeladat/src/main/java/hu/oe/javautolsooraifeladat/controller/RegisterServlet.java
@@ -1,5 +1,8 @@
 package hu.oe.javautolsooraifeladat.controller;
 
+import hu.oe.javautolsooraifeladat.model.User;
+import hu.oe.javautolsooraifeladat.model.UserRepository;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,6 +17,19 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String fullname = request.getParameter("fullname");
+        String address = request.getParameter("address");
 
+        User newuser = new User(username, password, fullname, address);
+
+        UserRepository users = (UserRepository) request.getServletContext().getAttribute("users");
+
+        users.addUser(newuser);
+
+        request.getSession().setAttribute("user", newuser);
+
+        request.getRequestDispatcher("mainpage.jsp").forward(request, response);
     }
 }
